dec928563327773d948fbee21773a5b1befe4edf
Bernardo Borras <bernardo.borras@hotmail.com>
2022-01-25 21:42:22 -0300

    Template para el panel de administracion
    Aca el profe nos muestra lo que sucede si hacemos que la funcion retorne una lista. Al hacer elsto ejecutamos maven y vamos a http://localhost:8080/prueba
    Y lo que nos figura es la lista de elementos en formato json

--- curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -4,13 +4,15 @@ package com.cursojava.curso.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UsuarioController {
 
 
     @RequestMapping(value="prueba")
-    public String preba() {
-        return "pruebita";
+    public List<String> prueba() {
+        return List.of("manzana", "kiwi", "banana");
     }
 
 }
