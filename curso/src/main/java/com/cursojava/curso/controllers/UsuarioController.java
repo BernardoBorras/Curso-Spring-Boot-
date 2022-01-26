258243ee1b62b2734633e3cc88243ea0031fe332
Bernardo Borras <bernardo.borras@hotmail.com>
2022-01-26 13:35:50 -0300

    Creacion de servicio Rest 2
    Creamos un metodo por cada funcion que queremos que tenga nuestro sistema.
    
    + Obtener Usuario
    + Editar Usuario
    + Registrar Usuario
    + Eliminiar Usuario

--- curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -10,10 +10,58 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-
+    // ---------------------------------------------- Metodo para retornar usuario en formato json
     @RequestMapping(value="usuario")    //     URL
 
     public Usuario getUsuario(){
+
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre("lucas");
+        usuario.setApellido("moy");
+        usuario.setEmail("lucas@gmail.com");
+        usuario.setTelefono("123456798");
+        usuario.setPassword("assas");
+
+        return usuario;
+    }
+
+    // ---------------------------------------------- Metodo para editar usuario
+    @RequestMapping(value="editar")    //     URL
+
+    public Usuario editar(){
+
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre("lucas");
+        usuario.setApellido("moy");
+        usuario.setEmail("lucas@gmail.com");
+        usuario.setTelefono("123456798");
+        usuario.setPassword("assas");
+
+        return usuario;
+    }
+
+    // ---------------------------------------------- Metodo para eliminar usuario
+    @RequestMapping(value="eliminar")    //     URL
+
+    public Usuario eliminar(){
+
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre("lucas");
+        usuario.setApellido("moy");
+        usuario.setEmail("lucas@gmail.com");
+        usuario.setTelefono("123456798");
+        usuario.setPassword("assas");
+
+        return usuario;
+    }
+
+    // ---------------------------------------------- Metodo para buscar usuario
+    @RequestMapping(value="buscar")    //     URL
+
+    public Usuario buscar(){
 
         Usuario usuario = new Usuario();
 
