d589f7bf0402a4bdf264a74f6ca40d19fee78a98
Bernardo Borras <bernardo.borras@hotmail.com>
2022-01-27 13:32:53 -0300

    Llamar a una api con AJAX 3
    Buscamos que al llamar a la url : http://localhost:8080/usuario
    nos retorne un listado de usuarios en json. Funciono perfecto.
    
    Luego cambiamos la funcion js para que reciba ese listado de usuarios en formato json que pudimos visualizar exitosamente.

--- curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -6,26 +6,64 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    // ---------------------------------------------- Metodo para retornar usuario en formato json
-    @RequestMapping(value="usuario/{id}")    //     URL
+    // -------------------------------------------- Metodo para retornar usuario en formato json
+    @RequestMapping(value="usuarios")    //  URL
 
-    public Usuario getUsuario(@PathVariable Long id){
+    public List<Usuario> getUsuarios(){
+
+        List<Usuario> usuarios = new ArrayList<>(); //   +++  creamos la lista
+
+
+        //-------------------------- creamos los usuarios
 
         Usuario usuario = new Usuario();
-
-        usuario.setId(id);
+        usuario.setId(1L); // como id es un atribut Long, le ponems L pa q no large error
         usuario.setNombre("lucasPrueba");
         usuario.setApellido("moy");
         usuario.setEmail("lucas@gmail.com");
         usuario.setTelefono("123456798");
         usuario.setPassword("assas");
 
-        return usuario;
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(2L);
+        usuario2.setNombre("ras");
+        usuario2.setApellido("algul");
+        usuario2.setEmail("lucas@gmail.com");
+        usuario2.setTelefono("123456798");
+        usuario2.setPassword("assas");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(3L);
+        usuario3.setNombre("majula");
+        usuario3.setApellido("moe");
+        usuario3.setEmail("lucas@gmail.com");
+        usuario3.setTelefono("123456798");
+        usuario3.setPassword("assas");
+
+        Usuario usuario4 = new Usuario();
+        usuario4.setId(4L);
+        usuario4.setNombre("nicolas");
+        usuario4.setApellido("hoy");
+        usuario4.setEmail("lucas@gmail.com");
+        usuario4.setTelefono("123456798");
+        usuario4.setPassword("assas");
+
+
+        //------------ Añadimos a los usuarios a la lista.
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        usuarios.add(usuario4);
+
+        return usuarios;     // ---- finaliza el metodo
+
     }
 
     // ---------------------------------------------- Metodo para editar usuario
--- curso/src/main/resources/static/js/usuarios.js
+++ curso/src/main/resources/static/js/usuarios.js
@@ -1,11 +1,6 @@
 
 // Call the dataTables jQuery plugin
-// Esto es una libreria de js
 
-// esta funcion hace que se ejecute todo lo que esta
-// denmtro de ella al segundo de estar cargandose la pagina.
-// Si ponemos una alaerta, va a mostrarla como primera accion
-// luego muestra la pagina.
 
 $(document).ready(function() {
 
@@ -14,21 +9,13 @@ $(document).ready(function() {
 
 });
 
-// que lo qu hace es seleccionar el elemento
-// con el tag #database y ponerle esta funcionalidad
-// .DataTable();   de convertir en una tabla que tenga
-//  paginacion y un monton de funcionalidades
+
 
 async function cargarUsuarios() {
 
-  const request = await fetch('usuario/234', {  // usuario/234 es la url de la funcion
-                                                    // getUsuario() que se encuentra en
-                                                    // UsusarioController.java. Enencargada
-                                                    // de devolvernos un json con los datos
-                                                    // del usuario. Lo que estamos haciendo
-                                                    // con esto es recibir esos datos desde
-                                                    // el back end y mostrarlos a travez de
-                                                    // una funcion .js en el usuarios.html
+  const request = await fetch('usuarios', {   // Esta es la url que manda a llamar de UsuarioController.java
+                                              //  la cual contiene el metodo getUsuarios() que escupe json.
+
     method: 'GET',
     headers: {
       'Accept': 'application/json',
