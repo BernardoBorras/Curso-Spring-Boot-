9c7a7247ad1423c5613efb12f4bc48cb2f3bed8f
Bernardo Borras <bernardo.borras@hotmail.com>
2022-02-11 13:49:38 -0300

    Lombock
    En la clase usuario vamos a agregar anotaciones para indicar que tabla tiene que utilizar la consulta realizada desde UsuarioDaoImpl.java
    
    En concreto, mediante anotaciones, establecimos que los atributos de la clase usuario, corresponden a columnas de la tabla usuarios. Asi quedan relacionadas, clase y tabla.
    
    Entonces ya quedaria lista la arquitectura para que desde UsuarioDaoImpl.java al solicitar datos de la clase Usuarios.java, hibernate entienda que esa clase esta relacionada con la tabla usuarios. Y de que manera.

--- curso/src/main/java/com/cursojava/curso/models/Usuario.java
+++ curso/src/main/java/com/cursojava/curso/models/Usuario.java
@@ -1,38 +1,42 @@
 package com.cursojava.curso.models;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-// Vamos a indicar con que tabla de la base de datos esta relacionada esta clase usuario.
-// Agregando anotaciones vamos a indicarle a esta clase , con que tabla esta relacionada.
+// Vamos a indicar Agregando anotaciones. Con que tabla de la bd esta relacionada esta clase usuario.
+// Para que al consultar la clase usuario, se acceda a la tabla usuarios, pues estan relacionadas.
 
 @Entity // Anotación que define que una clase se puede asignar a una tabla.
 @Table(name = "usuarios") // Con esto la clase sabe que va a utilizar esa tabla.
+
+// le estamos indicando que tabla estamos utilizando, pero hibernate no se da cuenta por
+// si solo, que columna es por su nombre.
+
 public class Usuario {
 
+    @Id    // Con esto le indicamos que esta columna va a contener la llave primaria.
+    @Getter @Setter @Column(name = "id")
     private Long id;
+
+    @Getter @Setter @Column(name = "nombre")
     private String nombre;
+
+    @Getter @Setter @Column(name = "apellido")
     private String apellido;
+
+    @Getter @Setter @Column(name = "email")
     private String email;
+
+    @Getter @Setter @Column(name = "telefono")
     private String telefono;
+
+    @Getter @Setter @Column(name = "password")
     private String password;
 
-    public Long getId() {return id;}
-    public void setId(Long id) {this.id = id;}
-
-    public String getNombre() {return nombre;}
-    public void setNombre(String nombre) {this.nombre = nombre;}
-
-    public String getApellido() {return apellido;}
-    public void setApellido(String apellido) {this.apellido = apellido;}
-
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
-
-    public String getTelefono() {return telefono;}
-    public void setTelefono(String telefono) {this.telefono = telefono;}
-
-    public String getPassword() {return password;}
-    public void setPassword(String password) {this.password = password;}
 }
